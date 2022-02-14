fe7a5ae8040caf4578cdf84b4e1f5c48cda1026d
Khoa Pham <onmyway133@gmail.com>
2022-02-14 20:48:51 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppStoreConnect",
+    platforms: [.iOS(.v13), .macCatalyst(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)],
+    products: [
+        .library(name: "AppStoreConnect", targets: ["AppStoreConnect"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kean/Get", from: "0.3.1"), 
+        .package(url: "https://github.com/kean/NaiveDate", from: "1.0.0"), 
+        .package(url: "https://github.com/kean/URLQueryEncoder", branch: "0.2.0")
+    ],
+    targets: [
+        .target(name: "AppStoreConnect", dependencies: [
+            .product(name: "Get", package: "Get"), 
+            .product(name: "NaiveDate", package: "NaiveDate"), 
+            .product(name: "URLQueryEncoder", package: "URLQueryEncoder")
+        ], path: "Sources")
+    ]
+)
